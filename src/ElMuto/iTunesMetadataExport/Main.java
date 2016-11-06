71270b23da93ad7a85010bee5edefc2511530bad
spengler <d1dPfImseTum>
2016-11-06 19:10:22 +0100

    outputting dom structure

--- src/ElMuto/iTunesMetadataExport/Main.java
+++ src/ElMuto/iTunesMetadataExport/Main.java
@@ -23,6 +23,9 @@ import org.w3c.dom.Node;
 
 public class Main {
 	
+	private static int indent = 0;
+	private static String basicIndent ="\t";
+	
 	private static void printlnCommon(Node n) {
 	    System.out.print(" nodeName=\"" + n.getNodeName() + "\"");
 
@@ -55,6 +58,106 @@ public class Main {
 	    }
 	    System.out.println();
 	}
+	
+	private static void outputIndentation() {
+	    for (int i = 0; i < indent; i++) {
+	        System.out.print(basicIndent);
+	    }
+	}
+	
+	private static void echo(Node n) {
+	    outputIndentation();
+	    int type = n.getNodeType();
+
+	    switch (type) {
+	        case Node.ATTRIBUTE_NODE:
+	        	System.out.print("ATTR:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.CDATA_SECTION_NODE:
+	        	System.out.print("CDATA:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.COMMENT_NODE:
+	        	System.out.print("COMM:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.DOCUMENT_FRAGMENT_NODE:
+	        	System.out.print("DOC_FRAG:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.DOCUMENT_NODE:
+	        	System.out.print("DOC:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.DOCUMENT_TYPE_NODE:
+	        	System.out.print("DOC_TYPE:");
+	            printlnCommon(n);
+	            NamedNodeMap nodeMap = ((DocumentType)n).getEntities();
+	            indent += 2;
+	            for (int i = 0; i < nodeMap.getLength(); i++) {
+	                Entity entity = (Entity)nodeMap.item(i);
+	                echo(entity);
+	            }
+	            indent -= 2;
+	            break;
+
+	        case Node.ELEMENT_NODE:
+	        	System.out.print("ELEM:");
+	            printlnCommon(n);
+
+	            NamedNodeMap atts = n.getAttributes();
+	            indent += 2;
+	            for (int i = 0; i < atts.getLength(); i++) {
+	                Node att = atts.item(i);
+	                echo(att);
+	            }
+	            indent -= 2;
+	            break;
+
+	        case Node.ENTITY_NODE:
+	        	System.out.print("ENT:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.ENTITY_REFERENCE_NODE:
+	        	System.out.print("ENT_REF:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.NOTATION_NODE:
+	        	System.out.print("NOTATION:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.PROCESSING_INSTRUCTION_NODE:
+	        	System.out.print("PROC_INST:");
+	            printlnCommon(n);
+	            break;
+
+	        case Node.TEXT_NODE:
+	        	System.out.print("TEXT:");
+	            printlnCommon(n);
+	            break;
+
+	        default:
+	        	System.out.print("UNSUPPORTED NODE: " + type);
+	            printlnCommon(n);
+	            break;
+	    }
+
+	    indent++;
+	    for (Node child = n.getFirstChild(); child != null;
+	         child = child.getNextSibling()) {
+	        echo(child);
+	    }
+	    indent--;
+	}
 
 	public static void main(String[] args) {
 
@@ -63,16 +166,15 @@ public class Main {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		
 		dbf.setNamespaceAware(true);
-	    dbf.setValidating(false);
+	    dbf.setValidating(true);
 	    
 	    DocumentBuilder db;
 		try {
 			db = dbf.newDocumentBuilder();
-			OutputStreamWriter errorWriter = new OutputStreamWriter(System.err,
-					"UTF-8");
 			Document doc = db.parse(new File(inputFileName));
 			
-			printlnCommon(doc.getFirstChild());
+			Node trackDict = doc.getFirstChild().getNextSibling();
+			echo(trackDict);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
