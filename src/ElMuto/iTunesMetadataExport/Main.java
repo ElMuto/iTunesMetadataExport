98e47edbb08b959761a1317799fe3c13bf2be2ae
spengler <d1dPfImseTum>
2016-11-13 12:59:13 +0100

    clean up

--- src/ElMuto/iTunesMetadataExport/Main.java
+++ src/ElMuto/iTunesMetadataExport/Main.java
@@ -12,12 +12,14 @@ import org.xml.sax.helpers.*;
 import java.io.File;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.DocumentType;
 import org.w3c.dom.Entity;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 
 
@@ -26,138 +28,30 @@ public class Main {
 	private static int indent = 0;
 	private static String basicIndent ="\t";
 	
-	private static void printlnCommon(Node n) {
-	    System.out.print(" nodeName=\"" + n.getNodeName() + "\"");
-
-	    String val = n.getNamespaceURI();
-	    if (val != null) {
-	    	System.out.print(" uri=\"" + val + "\"");
-	    }
-
-	    val = n.getPrefix();
-
-	    if (val != null) {
-	    	System.out.print(" pre=\"" + val + "\"");
-	    }
-
-	    val = n.getLocalName();
-	    if (val != null) {
-	    	System.out.print(" local=\"" + val + "\"");
-	    }
-
-	    val = n.getNodeValue();
-	    if (val != null) {
-	    	System.out.print(" nodeValue=");
-	        if (val.trim().equals("")) {
-	            // Whitespace
-	        	System.out.print("[WS]");
-	        }
-	        else {
-	        	System.out.print("\"" + n.getNodeValue() + "\"");
-	        }
-	    }
-	    System.out.println();
+	private class TitleMetadata {
+		public TitleMetadata(String title, String artist, int titleRating, Optional<Boolean> titleRatingComputed,
+				int albumRating, Optional<Boolean> albumRatingComputed) {
+			super();
+			this.title = title;
+			this.artist = artist;
+			this.titleRating = titleRating;
+			this.titleRatingComputed = titleRatingComputed;
+			this.albumRating = albumRating;
+			this.albumRatingComputed = albumRatingComputed;
+		}
+		private String title;
+		private String artist;
+		private int titleRating;
+		private Optional<Boolean> titleRatingComputed;
+		private int albumRating;
+		private Optional<Boolean>  albumRatingComputed;
+		
+		public void print() {
+			System.out.println(artist + " - " + title + ": " + titleRating) ;
+		}
 	}
 	
-	private static void outputIndentation() {
-	    for (int i = 0; i < indent; i++) {
-	        System.out.print(basicIndent);
-	    }
-	}
-	
-	private static void echo(Node n) {
-	    outputIndentation();
-	    int type = n.getNodeType();
 
-	    switch (type) {
-	        case Node.ATTRIBUTE_NODE:
-	        	System.out.print("ATTR:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.CDATA_SECTION_NODE:
-	        	System.out.print("CDATA:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.COMMENT_NODE:
-	        	System.out.print("COMM:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.DOCUMENT_FRAGMENT_NODE:
-	        	System.out.print("DOC_FRAG:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.DOCUMENT_NODE:
-	        	System.out.print("DOC:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.DOCUMENT_TYPE_NODE:
-	        	System.out.print("DOC_TYPE:");
-	            printlnCommon(n);
-	            NamedNodeMap nodeMap = ((DocumentType)n).getEntities();
-	            indent += 2;
-	            for (int i = 0; i < nodeMap.getLength(); i++) {
-	                Entity entity = (Entity)nodeMap.item(i);
-	                echo(entity);
-	            }
-	            indent -= 2;
-	            break;
-
-	        case Node.ELEMENT_NODE:
-	        	System.out.print("ELEM:");
-	            printlnCommon(n);
-
-	            NamedNodeMap atts = n.getAttributes();
-	            indent += 2;
-	            for (int i = 0; i < atts.getLength(); i++) {
-	                Node att = atts.item(i);
-	                echo(att);
-	            }
-	            indent -= 2;
-	            break;
-
-	        case Node.ENTITY_NODE:
-	        	System.out.print("ENT:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.ENTITY_REFERENCE_NODE:
-	        	System.out.print("ENT_REF:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.NOTATION_NODE:
-	        	System.out.print("NOTATION:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.PROCESSING_INSTRUCTION_NODE:
-	        	System.out.print("PROC_INST:");
-	            printlnCommon(n);
-	            break;
-
-	        case Node.TEXT_NODE:
-	        	System.out.print("TEXT:");
-	            printlnCommon(n);
-	            break;
-
-	        default:
-	        	System.out.print("UNSUPPORTED NODE: " + type);
-	            printlnCommon(n);
-	            break;
-	    }
-
-	    indent++;
-	    for (Node child = n.getFirstChild(); child != null;
-	         child = child.getNextSibling()) {
-	        echo(child);
-	    }
-	    indent--;
-	}
 
 	public static void main(String[] args) {
 
@@ -173,8 +67,28 @@ public class Main {
 			db = dbf.newDocumentBuilder();
 			Document doc = db.parse(new File(inputFileName));
 			
-			Node trackDict = doc.getFirstChild().getNextSibling();
-			echo(trackDict);
+			Node mainDict = doc.getChildNodes().item(1).getChildNodes().item(1);
+
+			Node currNode = mainDict.getFirstChild();
+			while (!currNode.getNodeName().equals("dict")) currNode = currNode.getNextSibling();
+			Node titleDict = currNode;
+			
+			NodeList titles = titleDict.getChildNodes();
+			for (int i = 0; i < titles.getLength(); i++) {
+				if (titles.item(i).getNodeName().equals("dict")) {
+					Node title = titles.item(i);
+					NodeList titleDataNodes = title.getChildNodes();
+					for (int j = 0; j < titleDataNodes.getLength(); j++) {
+						Node titleDataNode = titleDataNodes.item(j);
+						
+						String keyName;
+						if(titleDataNode.getNodeName().equals("key")) {
+							keyName = titleDataNode.getTextContent().trim();
+							System.out.println(keyName);
+						}
+					}
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
