8892953fa1553f8d55241253cffa2b1a507f34aa
spengler <d1dPfImseTum>
2016-11-13 13:28:57 +0100

    extraction of artist and title

--- src/ElMuto/iTunesMetadataExport/Main.java
+++ src/ElMuto/iTunesMetadataExport/Main.java
@@ -12,6 +12,8 @@ import org.xml.sax.helpers.*;
 import java.io.File;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.w3c.dom.Document;
@@ -72,21 +74,30 @@ public class Main {
 			Node currNode = mainDict.getFirstChild();
 			while (!currNode.getNodeName().equals("dict")) currNode = currNode.getNextSibling();
 			Node titleDict = currNode;
-			
+						
+			// iterate over titles
 			NodeList titles = titleDict.getChildNodes();
 			for (int i = 0; i < titles.getLength(); i++) {
 				if (titles.item(i).getNodeName().equals("dict")) {
 					Node title = titles.item(i);
-					NodeList titleDataNodes = title.getChildNodes();
-					for (int j = 0; j < titleDataNodes.getLength(); j++) {
-						Node titleDataNode = titleDataNodes.item(j);
-						
-						String keyName;
-						if(titleDataNode.getNodeName().equals("key")) {
-							keyName = titleDataNode.getTextContent().trim();
-							System.out.println(keyName);
+					
+					Map<String, String> attributeMap = new HashMap<>();
+					
+					// iterate over attributes
+					NodeList attributes = title.getChildNodes();
+					for (int j = 0; j < attributes.getLength(); j++) {
+						Node attribute = attributes.item(j);
+						if(attribute.getNodeName().equals("key")) {
+							String key = attribute.getTextContent().trim();
+							String value = attribute.getNextSibling().getTextContent();
+							
+//							System.out.println(key + " = " + value);
+							attributeMap.put(key, value);
+//							if (key.equals("Artist")) System.out.println(value);
 						}
 					}
+					
+					System.out.println(attributeMap.get("Artist") + " - " + attributeMap.get("Name"));
 				}
 			}
 		} catch (Exception e) {
