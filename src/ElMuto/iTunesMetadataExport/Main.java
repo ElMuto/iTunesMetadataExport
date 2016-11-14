81c33beb88f7d5b2cd6e0d73560c09055fff3384
ElMuto <helmut.spengler@tum.de>
2016-11-14 14:08:08 +0100

    removal of tags complete

--- src/ElMuto/iTunesMetadataExport/Main.java
+++ src/ElMuto/iTunesMetadataExport/Main.java
@@ -2,8 +2,14 @@ package ElMuto.iTunesMetadataExport;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +21,9 @@ import org.w3c.dom.NodeList;
 
 public class Main {
 	public static void main(String[] args) {
+		final boolean REMOVE_COMPUTED_RATINGS = true;
+		final boolean PRINT_TITLES_WITH_REMOVED_TAGS = false;
+		
 		String nullStr = "NULL";
 		String sep = "\t";
 
@@ -48,67 +57,121 @@ public class Main {
 				header += sep + interestingKeys[j];
 			}
 			
-			System.out.println(header);
+			if (!PRINT_TITLES_WITH_REMOVED_TAGS) {
+				System.out.println(header);
+			}
+			
 			// iterate over titles
 			NodeList titles = titleDict.getChildNodes();
 			for (int i = 0; i < titles.getLength(); i++) {
 				if (titles.item(i).getNodeName().equals("dict")) {
 					Node title = titles.item(i);
 					
-					Map<String, String> attributeMap = new HashMap<>();
+					Map<String, AttribNodePair> attributeMap = new HashMap<>();
 					
 					// iterate over attributes
 					NodeList attributes = title.getChildNodes();
 					for (int j = 0; j < attributes.getLength(); j++) {
-						Node attribute = attributes.item(j);
-						if(attribute.getNodeName().equals("key")) {
-							attributeMap.put(attribute.getTextContent().trim(), attribute.getNextSibling().getTextContent().trim());
+						Node attributeKeyNode = attributes.item(j);
+						if(attributeKeyNode.getNodeName().equals("key")) {
+							attributeMap.put(
+									attributeKeyNode.getTextContent().trim(),
+									new AttribNodePair(
+											attributeKeyNode.getNextSibling().getTextContent().trim(),
+											attributeKeyNode,
+											attributeKeyNode.getNextSibling()
+											)
+									);
 						}
 					}
 					
 					boolean interestingArtist = false;
 					for (int j = 0; j < interestingArtists.length && !interestingArtist; j++) {
-						if (attributeMap.containsKey("Artist") && attributeMap.get("Artist").equals(interestingArtists[j])) {
+						if (attributeMap.containsKey("Artist") && attributeMap.get("Artist").getValue().equals(interestingArtists[j])) {
 							interestingArtist = true;
 						}
 					}
 					
 					boolean interestingTitle = false;
 					for (int j = 0; j < interestingTitles.length && !interestingTitle; j++) {
-						if (attributeMap.get("Name").equals(interestingTitles[j])) {
+						if (attributeMap.get("Name").getValue().equals(interestingTitles[j])) {
 							interestingTitle = true;
 						}
 					}
 
 					boolean ratingComputed 		= attributeMap.containsKey("Rating Computed") 		? true : false;
 					boolean albumRatingComputed = attributeMap.containsKey("Album Rating Computed") ? true : false;
-
-//					if (ratingComputed) {
-					if (interestingArtist && interestingTitle) {
-						String row = attributeMap.containsKey(interestingKeys[0]) ? attributeMap.get(interestingKeys[0]) : nullStr;					
-						for (int j = 1; j < interestingKeys.length; j++) {
-							row += sep;
-							String key = interestingKeys[j];
-							
-							if (key.equals("Rating Computed")) {
-								row += String.valueOf(ratingComputed);
-							} else	if (key.equals("Album Rating Computed")) {
-								row += String.valueOf(albumRatingComputed);
-							} else {
-								if (attributeMap.containsKey(key)) {
-									row += attributeMap.get(key);
-								} else {
-									row += nullStr;
-								}
-							}
+					
+					if (REMOVE_COMPUTED_RATINGS && ratingComputed) {
+						
+						title.removeChild(attributeMap.get("Rating").getKeyNode());
+						title.removeChild(attributeMap.get("Rating").getValueNode());
+						
+						if (PRINT_TITLES_WITH_REMOVED_TAGS) {
+						System.out.println("Removed \"" + attributeMap.get("Name").getValue() + "\" by " + 
+								(attributeMap.containsKey("Artist") ? ("\"" +  attributeMap.get("Artist").getValue() + "\"") : "unknown artist"));
 						}
-						System.out.println(row);
+					}
+					
+					if (interestingArtist && interestingTitle  && !PRINT_TITLES_WITH_REMOVED_TAGS) {
+						printTitleInfoToConsole(nullStr, sep, interestingKeys, attributeMap, ratingComputed,
+								albumRatingComputed);
 					}
 				}
 			}
+			Transformer tx = TransformerFactory.newInstance().newTransformer();
+			tx.setOutputProperty(OutputKeys.INDENT, "no");
+			tx.transform(new DOMSource( doc), new StreamResult(new FileOutputStream("data/iTunes Music Library.clean.xml")));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	private static void printTitleInfoToConsole(String nullStr, String sep, String[] interestingKeys,
+			Map<String, AttribNodePair> attributeMap, boolean ratingComputed, boolean albumRatingComputed) {
+		String row = attributeMap.containsKey(interestingKeys[0]) ? attributeMap.get(interestingKeys[0]).getValue() : nullStr;					
+		for (int j = 1; j < interestingKeys.length; j++) {
+			row += sep;
+			String key = interestingKeys[j];
+			
+			if (key.equals("Rating Computed")) {
+				row += String.valueOf(ratingComputed);
+			} else	if (key.equals("Album Rating Computed")) {
+				row += String.valueOf(albumRatingComputed);
+			} else {
+				if (attributeMap.containsKey(key)) {
+					row += attributeMap.get(key).getValue();
+				} else {
+					row += nullStr;
+				}
+			}
+		}
+		System.out.println(row);
+	}
+
+	private static class AttribNodePair {
+		
+		private final String value;
+		private final Node keyNode;
+		private final Node valueNode;
+		
+		public AttribNodePair(String value, Node keyNode, Node valueNode) {
+			super();
+			this.value = value;
+			this.keyNode = keyNode;
+			this.valueNode = valueNode;
+		}
+		
+		public String getValue() {
+			return value;
+		}
+		
+		public Node getKeyNode() {
+			return keyNode;
+		}
+		
+		public Node getValueNode() {
+			return valueNode;
+		}
+	}
 }
